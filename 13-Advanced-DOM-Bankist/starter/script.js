a5c686dc37f0e4aede5ca663c64a8fe4c2f6f29c
dhravmadan22 <63240841+dhravmadan22@users.noreply.github.com>
2023-09-08 03:59:41 +0530

    Reveal elements on scroll, lazy loading images

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -192,6 +192,51 @@ observer.observe(header);
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 
+// Revealing Elements on scroll
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries; // same as entry = entry[0]
+  // console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+// LAZY LOADING IMAGES
+const imgTargets = document.querySelectorAll('img[data-src]');
+// console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0.75,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
 // <------------------------------------------------------------->
 // <------------------------------------------------------------->
 
