b0d634f1151ee07b08cdc44d0c14f3f1b21d38c6
dhravmadan22 <63240841+dhravmadan22@users.noreply.github.com>
2023-09-09 16:29:30 +0530

    Project completed, defer, async and different load events

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -487,3 +487,31 @@ slider();
 // [...h1.parentElement.children].forEach(function (el) {
 //   if (el !== h1) el.style.transform = 'scale(0.5)';
 // });
+
+/////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// Lifecycle DOM Events
+
+// DOM content loaded
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page Fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log('Before Unload', e);
+//   e.returnValue = '';
+// });
+
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+// Efficient Script loading
+
